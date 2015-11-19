3683c80b5e9975e7fd0d5d781c0e125e4710d991
Sebastain Misas <juanmisas2@gmail.com>
2015-11-19 11:14:11 -0500

    Wiring was succesful matching pokemon id with pokimon api

--- app/assets/javascripts/pokemon.js
+++ app/assets/javascripts/pokemon.js
@@ -1,2 +1,37 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+PokemonApp.Pokemon = function (pokemonUri) {
+	// 							// api/v1/19
+	this.id = PokemonApp.Pokemon.idFromUri(pokemonUri);
+};
+
+
+PokemonApp.Pokemon.prototype.render = function () {
+	console.log("Rendering pokemon: #" + this.id);
+	$.ajax({
+		url: "/api/pokemon/" + this.id,
+		success: function (responce) {
+			console.log(responce);
+		}
+		error: function() {
+			alert("You fucked up!");
+		}
+	});
+};
+
+PokemonApp.Pokemon.idFromUri = function (PokemonUri) {
+	var uriSegments = PokemonUri.split("/");
+	var secondLast = uriSegments.length - 2;
+	return uriSegments[secondLast];
+};
+
+
+$(document).on("ready", function (){
+	$(".js-show-pokemon").on("click", function (event){
+		var $button = $(event.currentTarget);
+		var pokemonUri = $button.data("pokemon-uri");
+
+		var pokemon = new PokemonApp.Pokemon(pokemonUri);
+		pokemon.render();
+	});
+});
